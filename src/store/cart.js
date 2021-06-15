65cd222bb74993183566e6ff6e07ed66157a0e9a
Anthony <anthony.a.s.martin@gmail.com>
2021-06-15 17:46:06 -0400

    start phase 3

---
+++ src/store/cart.js
@@ -0,0 +1,15 @@
+// const getProduce = ()
+//  // grab the state, the name of the state reducer, then the name of the key in the state
+//  const articles = useSelector(state=>state.articleState.articles)
+
+import { useSelector } from "react-redux";
+
+// const produce = useSelector(state =>state.produce.)
+export default function cartReducer(state ={}, action){
+    switch(action.type){
+       
+        default:
+            return state;
+
+    }
+}
